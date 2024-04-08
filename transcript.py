0f56c01b75974f9ed80677792fbc96f5fe757673
Ernster24 <ernieg2409@gmail.com>
2024-04-08 17:55:56 +0100

    First commit

---
+++ transcript.py
@@ -0,0 +1,32 @@
+import speech_recognition as speech
+import sys
+
+r = speech.Recognizer()
+
+def transcribe(audio_file, file_no):
+    with speech.AudioFile(audio_file) as source:
+        audio_data = r.record(source)
+
+        try:
+            text = r.recognize_google(audio_data)
+            return text
+        except:
+            return "!!!!Error Transcribing Audio!!!!"
+            
+
+        #print("Writing '" + text + "' to file...")
+        #transcript = open("transcript.txt", "a")
+        #transcript.write("[" + str(file_no) + "] " + text + "\n")
+        #transcript.close()
+        
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python transcript.py <audio_file> <file_no>")
+        
+    audio_file = sys.argv[1]
+    file_no = sys.argv[2]
+    
+    transcribe(audio_file, file_no)
+  
